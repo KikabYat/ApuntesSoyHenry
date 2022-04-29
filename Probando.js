c2ada36f5bcdd3dec7c3ce49569ec3c540b47afa
Kikab <kikabyat@gmail.com>
2022-04-29 11:24:17 -0600

    Otro commit guardado 29-abril-2022

---
+++ Probando.js
@@ -0,0 +1,22 @@
+var toni = "Toni";
+
+function Hola(){
+    console.log(toni);
+}
+
+Hola();
+
+
+alert("Hola");
+
+
+var lugarNa = "El Estor";
+
+function darLugar(){
+    console.log(lugarNa);
+}
+
+darLugar();
+
+
+Math.max(12,31,54,12,45,6,1213165);
